36bdc19479eb03f796d9a70aa2073a9a09c96373
tsumakovamilana <tsumakovamilana955@gmail.com>
2025-01-08 11:34:15 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kutse_App_DM.Startup))]
+namespace Kutse_App_DM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
